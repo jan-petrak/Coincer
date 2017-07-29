85a8165a77ae2de584565b2bcad20698eda28b89
petrak <456484@mail.muni.cz>
2017-07-29 17:44:26 +0200

    Base of a libevent loop

--- src/coincerd.c
+++ src/coincerd.c
@@ -16,6 +16,10 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
+
+#include "p2p.h"
+
 int main(void)
 {
 	/*
@@ -35,6 +39,10 @@ int main(void)
 	 *
 	 * - terminate on SIGTERM
 	 */
+
+	int r;
+	if ((r = listen_init()) != 0)
+		return r;
 
 	return 0;
 }
